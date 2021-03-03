c29d8288e7065155debb3147c7bd8637adc26ae5
huming <huming@missfresh.cn>
2021-03-03 22:23:14 +0800

    delete

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,7 +1,25 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+import org.junit.Test;
+
+import io.netty.channel.nio.NioEventLoop;
+import io.netty.channel.nio.NioEventLoopGroup;
 
 public class Test2 {
 
-	public static void main(String[] args) {
-		System.out.println(Integer.toBinaryString(128));
+	@Test
+	public void test1() throws InterruptedException, ExecutionException {
+		NioEventLoop eventloop = (NioEventLoop) new NioEventLoopGroup(1).next();
+		FutureTask<String> task = new FutureTask<String>(new Callable<String>() {
+			@Override
+			public String call() throws Exception {
+				Thread.sleep(3000);
+				return "huming";
+			}
+		});
+		eventloop.execute(task);
+		System.out.println(task.get());
 	}
 }
